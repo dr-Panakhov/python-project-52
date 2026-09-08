7202c1795814b04581ea8ac6709b1bf15a6e0f96
dr-Panakhov <rafael.panaxov14@gmail.com>
2026-09-08 23:26:50 +0400

    auto fix

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -10,10 +10,16 @@ class RegisterUserForm(UserCreationForm):
         model = User
         fields = ('first_name', 'last_name', 'username')
 
-class UserUpdateForm(UserChangeForm):
+class UserUpdateForm(UserCreationForm):
     first_name = forms.CharField(label='Имя', max_length=150, required=True)
     last_name = forms.CharField(label='Фамилия', max_length=150, required=True)
 
-    class Meta(UserChangeForm.Meta):
+    class Meta(UserCreationForm.Meta):
         model = User
         fields = ('first_name', 'last_name', 'username')
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError("Пользователь с таким именем уже существует.")
+        return username
