247a8c209f7253c6b4b9328f0baae6f97374e0b3
dr-Panakhov <rafael.panaxov14@gmail.com>
2026-09-08 23:17:30 +0400

    auto fix

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from .models import Task
+from django.contrib.auth.models import User
 
 class TaskForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['executor'].label_from_instance = lambda user: f"{user.first_name} {user.last_name}"
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
